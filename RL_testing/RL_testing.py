a04de0f62783815d92cb00f913fd3852eb0c3222
iandashevskyi <dashevskiyan@gmail.com>
2025-10-04 20:43:08 +0700

    random_seed

--- RL_testing/RL_testing.py
+++ RL_testing/RL_testing.py
@@ -1,14 +1,27 @@
 ﻿import gymnasium as gym
+import time
 
-env = gym.make("CartPole-v1", render_mode = "human")
-observation, info = env.reset()
-print(f"Starting observation: {observation}")
+env = gym.make("CartPole-v1", render_mode="human")
 
-episode_over = False
+observation, info = env.reset(seed=42)
+env.action_space.seed(42)
 total_reward = 0
-while (not episode_over):
+
+for _ in range(200):
+
+    time.sleep(0.05)
+
     action = env.action_space.sample()
+
     observation, reward, terminated, truncated, info = env.step(action)
 
-    total_reward+=reward
-    episode_over = terminated or truncated
+    total_reward += reward
+
+    print(f"Действие: {action}, Награда за шаг: {reward}, Суммарная награда: {total_reward}")
+
+    if terminated or truncated:
+        print("\nЭпизод завершен!")
+        break
+
+env.close()
+print(f"Итоговая награда за весь эпизод: {total_reward}")
